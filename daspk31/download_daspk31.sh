1f7f3114dd22489e386d5d2992ad5ece6d5a6031
Richard West <r.west@neu.edu>
2015-02-23 19:33:45 -0500

    daspk31 Makefile now downloads if needed.
    This simplifies the installation to::
    
    	$ make
    	$ make daspk
    	$ make install
    
    I still think the target "daspk" is weird, as there's already the daspk2 and this
    is just for daspk3, but that's another issue.

--- daspk31/download_daspk31.sh
+++ daspk31/download_daspk31.sh
@@ -1,5 +1,10 @@
-echo 'Downloading and unpacking DASPK 3.1 fortran source files...'
-wget http://www.cs.ucsb.edu/~cse/Software/daspk31.tgz
-tar zxvf daspk31.tgz -C ../
-rm daspk31.tgz
+#!/bin/bash
+read -p "Do you agree to DASPK3.1 copyright restrictions at http://www.cs.ucsb.edu/~cse?" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo 'Downloading and unpacking DASPK 3.1 fortran source files...'
+    wget http://www.cs.ucsb.edu/~cse/Software/daspk31.tgz
+    tar zxvf daspk31.tgz -C ../
+fi 
 
