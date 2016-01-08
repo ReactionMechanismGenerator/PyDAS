bbf990745d17f88df30c94b4053d5d9d7eaaedba
Connie Gao <connieg@mit.edu>
2016-01-08 11:43:20 -0500

    Fixes to PyDAS windows build for conda

--- bld.bat
+++ bld.bat
@@ -1,4 +1,5 @@
-::wget -P daspk31 http://www.engineering.ucsb.edu/~cse/Software/daspk31.tgz
+wget -P daspk31 http://www.engineering.ucsb.edu/~cse/Software/daspk31.tgz
+tar zxvf daspk31/daspk31.tgz
 ::mingw32-make
 
 ::gfortran -O3 -c dassl/daux.f -o dassl/daux.o
@@ -6,7 +7,7 @@
 ::gfortran -O3 -c dassl/dlinpk.f -o dassl/dlinpk.o
 ::ar rcs dassl/libddassl.a dassl/daux.o dassl/ddassl.o dassl/dlinpk.o
 ::echo "PREFIX"
-cp %PREFIX%\..\..\libs\libpython27.a %PREFIX%\libs
+cp "%PREFIX%\..\..\libs\libpython27.a" "%PREFIX%\libs\libpython27.a"
 
 
 cd dassl
@@ -18,10 +19,10 @@ mingw32-make
 cd ..
 
 ::echo Compiling PyDAS...
-%PYTHON% setup.py build_ext daspk --compiler=mingw32 --inplace
-%PYTHON% setup.py daspk install
+"%PYTHON%" setup.py build_ext daspk --compiler=mingw32 --inplace
+"%PYTHON%" setup.py daspk install
 
 ::%PYTHON% setup.py daspk install
 
 :: Save version number stored in pydas/__init__.py file
-%PYTHON% -c "from pydas import __version__; print __version__" > %SRC_DIR%/__conda_version__.txt
+"%PYTHON%" -c "from pydas import __version__; print __version__" > "%SRC_DIR%/__conda_version__.txt"
