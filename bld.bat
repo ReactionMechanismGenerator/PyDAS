3d4b9d5f28c5bd3a94fe7c7a58951c8924a139e7
Connie Gao <connieg@mit.edu>
2016-01-08 14:12:27 -0500

    Add instructions to windows anaconda build file bld.bat
    Instructions for Anaconda build in windows
    
    First you must install mingwpy for anaconda using the command
    $ pip install -i https://pypi.anaconda.org/carlkl/simple mingwpy
    
    You need to create the libpython27.a needed for the gfortran compiler
    
    In the ~/Anaconda folder, type the following in the command line:
    $ gendef python27.dll
    $ dlltool -D python27.dll -d python27.def -l libs/libpython27.a
    
    Then point distutils to MinGW
    $ echo -e "[build]\ncompiler=mingw32" > /c/Python27/Lib/distutils/distutils.cfg
    
    Carrying out these steps will allow you to avoid the Microsoft Visual Studio C++ Compiler
    and rely solely on GNU MinGW.  This is because it is very difficult to link
    the necessary gfortran compiler with Visual Studio.

--- bld.bat
+++ bld.bat
@@ -1,3 +1,22 @@
+:: Instructions for Anaconda build in windows
+::
+:: First you must install mingwpy for anaconda using the command
+::  $ pip install -i https://pypi.anaconda.org/carlkl/simple mingwpy
+::
+:: You need to create the libpython27.a needed for the gfortran compiler
+::
+:: In the ~/Anaconda folder, type the following in the command line:
+::  $ gendef python27.dll
+::  $ dlltool -D python27.dll -d python27.def -l libs/libpython27.a
+::
+:: Then point distutils to MinGW
+::  $ echo -e "[build]\ncompiler=mingw32" > /c/Python27/Lib/distutils/distutils.cfg
+::
+:: Carrying out these steps will allow you to avoid the Microsoft Visual Studio C++ Compiler
+:: and rely solely on GNU MinGW.  This is because it is very difficult to link
+:: the necessary gfortran compiler with Visual Studio. 
+
+
 wget -P daspk31 http://www.engineering.ucsb.edu/~cse/Software/daspk31.tgz
 tar zxvf daspk31/daspk31.tgz
 
