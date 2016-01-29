3aa41959805aebf3562b69a7ff1a98f878b96254
Connie Gao <connieg@mit.edu>
2016-01-29 10:28:28 -0800

    Add some changes to the conda build for linux-32 gcc problem
    gcc-4.8.5-3 conda package does not install properly for linux-32 builds.
    gcc-4.8.2 does work, but does not properly link libraries with used in
    conda-build.  Therefore you have to install on base anaconda and copy
    those libraries over.  Uncomment lines in build.sh for use with linux-32

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
+# Due to problems with linux32 gcc, you must first install gcc 4.8.2 onto your root anaconda build.  Then physically copy over the library files
+# See http://github.com/conda/conda-recipes/issues/510
+
+# Uncomment the following lines for linux-32
+# rm ${PREFIX}/lib/gcc -r
+# mkdir $PREFIX/lib/gcc
+# cp -r (insert_base_anaconda_path_here)/lib/gcc/* ${PREFIX}/lib/gcc/
+
+
 export CC=${PREFIX}/bin/gcc
 export CXX=${PREFIX}/bin/g++
 export F77=${PREFIX}/bin/gfortran
