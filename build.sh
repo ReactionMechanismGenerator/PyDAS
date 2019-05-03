dd76b2fc59c567b12aa50d6b4f7360f61d7a63c2
Max Liu <mjliu@mit.edu>
2019-05-03 17:36:15 -0400

    Update conda recipe and unix build script for new conda-build and py3

--- build.sh
+++ build.sh
@@ -1,19 +1,9 @@
-# Due to problems with linux32 gcc, you must first install gcc 4.8.2 onto your root anaconda build.  Then physically copy over the library files
-# See http://github.com/conda/conda-recipes/issues/510
+# Download DASPK 3.1
+wget -P daspk31 https://sites.engineering.ucsb.edu/~cse/Software/daspk31.tgz
 
-# Uncomment the following lines for linux-32
-# rm ${PREFIX}/lib/gcc -r
-# mkdir $PREFIX/lib/gcc
-# cp -r (insert_base_anaconda_path_here)/lib/gcc/* ${PREFIX}/lib/gcc/
-
-
-export CC=${PREFIX}/bin/gcc
-export CXX=${PREFIX}/bin/g++
-export F77=${PREFIX}/bin/gfortran
-wget -P daspk31 http://www.engineering.ucsb.edu/%7Ecse/Software/daspk31.tgz
+# Compile solvers
 make
 
+# Build cython wrapper
 $PYTHON setup.py daspk install
 
-# Save version number stored in pydas/__init__.py file
-$PYTHON -c 'from pydas import __version__; print __version__' > ${SRC_DIR}/__conda_version__.txt
