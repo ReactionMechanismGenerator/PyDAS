966ca62583ecaf745f8bba7386094f7df9cc1091
Max Liu <mjliu@mit.edu>
2019-07-14 21:16:29 -0400

    Move conda recipe to .conda folder and adjust daspk31 install
    Putting recipe in .conda folder allows building the package
    without unnecessarily including all of the source files
    
    Instead of downloading daspk31 as part of the build, assume
    the tar file is already in the daspk31 directory

--- build.sh
+++
@@ -1,9 +0,0 @@
-# Download DASPK 3.1
-wget -P daspk31 https://sites.engineering.ucsb.edu/~cse/Software/daspk31.tgz
-
-# Compile solvers
-make
-
-# Build cython wrapper
-$PYTHON setup.py daspk install
-
